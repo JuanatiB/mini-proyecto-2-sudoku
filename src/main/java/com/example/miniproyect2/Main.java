5f822b5ef0c77d5e7fb39816646ce480e4b88963
Juan Natib <juan.natib@correounivalle.edu.co>
2024-10-15 20:58:33 -0500

    Project creation

---
+++ src/main/java/com/example/miniproyect2/Main.java
@@ -0,0 +1,11 @@
+package com.example.miniproyect2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        launch();
+    }
+}
