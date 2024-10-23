f4528daf938de533c7373bdeb42728a14ebd9740
Manuel <juan.ampudia@correounivalle.edu.co>
2024-10-23 14:23:15 -0600

    solved gamestage error

--- src/main/java/com/example/miniproyect2/controller/ErrorDisplay.java
+++ src/main/java/com/example/miniproyect2/controller/ErrorDisplay.java
@@ -1,4 +1,15 @@
 package com.example.miniproyect2.controller;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class ErrorDisplay {
+
+    public static void showError(String title, String message) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null); // Puedes agregar un encabezado si lo deseas
+        alert.setContentText(message);
+        alert.showAndWait(); // Muestra la alerta y espera a que el usuario la cierre
+    }
 }
