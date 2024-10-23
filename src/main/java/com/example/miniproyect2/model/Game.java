f59f8060f74bddd5a5302ca747f8f4cb15365d91
Juan Natib <juan.natib@correounivalle.edu.co>
2024-10-23 17:47:38 -0500

    Game interface creation and implementation

--- src/main/java/com/example/miniproyect2/model/Game.java
+++ src/main/java/com/example/miniproyect2/model/Game.java
@@ -1,5 +1,48 @@
 package com.example.miniproyect2.model;
 
+import java.util.ArrayList;
+
 public class Game implements IGame {
-    private int cantHints = 3;
+    private int[][] board = {
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0}
+    };
+
+    private ArrayList<Integer> block1;
+    private ArrayList<Integer> block2;
+    private ArrayList<Integer> block3;
+    private ArrayList<Integer> block4;
+    private ArrayList<Integer> block5;
+    private ArrayList<Integer> block6;
+
+
+
+    @Override
+    public void AddToBoard(int row, int col, int value) {
+        board[row][col] = value;
+    }
+
+    @Override
+    public void fillBlock(int[][] board, int block) {
+
+    }
+
+    @Override
+    public boolean verifyRow(int row) {
+        return false;
+    }
+
+    @Override
+    public boolean verifyColumn(int col) {
+        return false;
+    }
+
+    @Override
+    public boolean verifyBlock(int index) {
+        return false;
+    }
 }
--- src/main/java/com/example/miniproyect2/model/IGame.java
+++ src/main/java/com/example/miniproyect2/model/IGame.java
@@ -1,4 +1,10 @@
 package com.example.miniproyect2.model;
 
 public interface IGame {
+    void AddToBoard(int r, int c, int value);
+    void fillBlock(int[][] board, int block);
+    boolean verifyRow(int index);
+    boolean verifyColumn(int index);
+    boolean verifyBlock(int index);
+
 }
